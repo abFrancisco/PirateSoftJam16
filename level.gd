a1872a859317fd58335fb1017ad9d2027876e285
Francisco Bernardo <francisco.bernardo.95@gmail.com>
2025-01-30 16:02:19 +0000

    added building_blocks folder containing premade blocks for level building

--- level.gd
+++ level.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var wall_draw : Array[PackedVector2Array]
+var terrain_draw : Array[PackedVector2Array]
 var tessellate_degrees : float = 1.0
 var max_stages : int = 5
 var ray_point : Vector2 = Vector2.ZERO
@@ -8,7 +8,15 @@ var ray_point : Vector2 = Vector2.ZERO
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_graph_duplicate"):
-		get_tree().reload_current_scene()
+		for child in %TerrainGen.get_children():
+			print("terrain gen has child = "+str(child))
+			child.queue_free()
+		terrain_draw.clear()
+		queue_redraw()
+		print("before await")
+		await get_tree().create_timer(1.0).timeout
+		print("after await")
+		print_exec_time(process_terrain)
 
 func _ready():
 	print_exec_time(process_terrain)
@@ -26,26 +34,28 @@ func print_exec_time(function:Callable) -> void:
 	print(str(function.get_method())+" took "+str(Time.get_ticks_usec()-start_time)+"μs to run")
 
 func process_terrain() -> void:
-	for child in %Terrain.get_children():
+	for child in %TerrainEdit.get_children():
 		var baked_points:PackedVector2Array=child.curve.get_baked_points()
 		var offset_points:PackedVector2Array=PackedVector2Array()
 		var offset_position:Vector2=child.position
 		var offset_scale:Vector2=child.scale
 		var offset_rotation:float=child.rotation
 		for point in baked_points:
-			offset_points.append(((point*offset_scale).rotated(offset_rotation))+offset_position)
-		wall_draw.append(offset_points)
+			point=point*offset_scale
+			point=point.rotated(offset_rotation)
+			point=point+offset_position
+			offset_points.append(point)
+		terrain_draw.append(offset_points)
 	queue_redraw()
 
 func _draw():
-	var i = 0
-	for group_points in wall_draw:
-		i += 1
+	for group_points in terrain_draw:
 		draw_colored_polygon(group_points, Color("0f0f19"))
-		draw_polyline(group_points, Color(1, 1, 1, 1), 0.8, true)
+		#polyline is outline, currently same color because polyline has anti aliasing which draw_colored_polygon doesnt have
+		draw_polyline(group_points, Color("0f0f19"), 0.8, true)
 		var wall_body = StaticBody2D.new()
 		var collision_shape = CollisionPolygon2D.new()
 		collision_shape.build_mode = CollisionPolygon2D.BUILD_SEGMENTS
 		collision_shape.set_polygon(group_points)
 		wall_body.add_child(collision_shape)
-		add_child(wall_body)
+		%TerrainGen.add_child(wall_body)
