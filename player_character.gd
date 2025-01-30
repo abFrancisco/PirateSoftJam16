cc2d279524a4c40242ae79f16c76d6eaf8ed1001
Francisco Bernardo <francisco.bernardo.95@gmail.com>
2025-01-30 20:26:00 +0000

    refactored player_character movement
    - it is getting there..

--- player_character.gd
+++ player_character.gd
@@ -2,14 +2,22 @@ extends Node2D
 
 var angle:float=0.0
 var direction:Vector2=Vector2.ZERO
+var swing_force:float=50000
+var suction_force:float=50000
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var suction=Input.is_action_pressed("ui_accept")
 	angle=%Knight.position.angle_to_point(get_global_mouse_position())
 	direction=Vector2.RIGHT.rotated(angle)
-	%SwordBody.apply_central_impulse(direction*10000*delta)
+	%Sword.apply_central_impulse(direction*swing_force*delta)
+	if suction:
+		#%Sword.apply_central_impulse((%Knight.position-%Sword.position).normalized()*suction_force*delta)
+		%Knight.mass=10
+	else:
+		%Knight.mass=50
 	queue_redraw()
 
 func _draw():
