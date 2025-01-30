5d1c0855b1baaf5620d7b4a952754e3bed2f65bc
Francisco Bernardo <francisco.bernardo.95@gmail.com>
2025-01-30 17:55:31 +0000

    added movement to player_character
    - needs lots of tuning

--- player_character.gd
+++ player_character.gd
@@ -9,10 +9,8 @@ var direction:Vector2=Vector2.ZERO
 func _process(delta):
 	angle=%Knight.position.angle_to_point(get_global_mouse_position())
 	direction=Vector2.RIGHT.rotated(angle)
-	print("angle      = " + str(rad_to_deg(angle)))
-	print("direction = " + str(direction.normalized()))
-	#%SwordBody.apply_central_force()
+	%SwordBody.apply_central_impulse(direction*10000*delta)
 	queue_redraw()
 
 func _draw():
-	draw_line(%Knight.position, %Knight.position+Vector2.RIGHT.rotated(angle)*100,Color.RED, 5.0, true)
+	draw_line(%Knight.position, %Knight.position+direction*100,Color.RED, 5.0, true)
